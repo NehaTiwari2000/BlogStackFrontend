235f7bf26776660cacbaf13dddf8dcf4347cfd46
atiyaachoudhary <atiya.choudhary@neosoftmail.com>
2023-07-11 11:55:13 +0530

    blog and stack page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -14,6 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+
 // C:\Users\user\3D Objects\BloStack Repo\BlogStackFrontend\node_modules\sweetalert2
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     HomeComponent,
     LoginComponent,
     NavbarComponent,
+    BlogsComponent
 
   ],
   imports: [
