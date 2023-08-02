6b54cdddb321c7bb0c18d633bcad1b79dbc24b60
arbaazsayedneo <138784466+arbaazsayedneo@users.noreply.github.com>
2023-08-02 12:49:33 +0530

    Added changes to auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,7 +14,7 @@ import { catchError} from 'rxjs/operators';
 export class AuthService {
   // baseUrl = 'http://ec2-18-212-53-8.compute-1.amazonaws.com:9091/v1.0/authentication'
   
-  baseUrl = 'http://localhost:9091/v1.0/authentication'
+  baseUrl = 'http://localhost:8080/v1.0/authentication'
 
   constructor(private httpClient: HttpClient) {
   }
